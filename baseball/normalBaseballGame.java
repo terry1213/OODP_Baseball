0b4bb5798e4fd817aa6b493ec6adba987888871e
geun12 <44759115+geun12@users.noreply.github.com>
2019-06-02 15:36:06 +0900

    Add files via upload

--- baseball/normalBaseballGame.java
+++ baseball/normalBaseballGame.java
@@ -1,50 +1,18 @@
 package baseball;
 
-import java.io.PrintStream;
-import java.util.ArrayList;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
+
 
 public class normalBaseballGame extends baseballGame{	
 
-	private JPanel contentPane;
-	
-	
-	private team t1;   //팀 1번 
-	private team t2;   //팀 2번 
-	
 	private team currentTeam = new team();    //현재 팀에 새로운 팀 할당.
-	private ArrayList<String> resultList;     //공을 던진 후의 결과를 모아놓은 리스트.
-	
-	private int inning;  //현재 이닝.
-
-	private boolean isGameOver;  //게임 종료 여부.
 	private int strike;  //스트라이크
 	private int ball; // 볼.
 	private int out; // 아웃
 	
 	private String msg1; //메시지를 담는 변수. 
 	private int gameFlag; //0이 되면 체인지, 1이 되면 
-	private int[][] scoreBoard = new int[2][11];	//스코어보드
-	private int[] scoreSum = new int[2];
-	
-	private JFrame frame = new JFrame();
-    
-
-    private JTextArea ta = new JTextArea();
-    private TextAreaOutputStream taos = new TextAreaOutputStream( ta, 1000 );
-    private PrintStream ps = new PrintStream( taos );
-	
-
-	private field fieldPanel;
-  
- 
-	
-
-
-
 //	-----------constructor for normal game--------------
 	public normalBaseballGame(JPanel panel){	
 		super(panel);
